7441ed85b00f269b659a22c3560f410b7f7be6d5
root <root@ip-172-16-1-10.ap-northeast-1.compute.internal>
2025-01-02 08:18:04 +0000

    legend_commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2'); // mysql2를 권장
 
 const db = mysql.createConnection({
-  host: 'terraform-20241227060603127100000002.cxouwm6o45aj.ap-northeast-1.rds.amazonaws.com',  // MySQL 서버 IP
+  host: '',  // MySQL 서버 IP
   user: 'admin',           // MySQL 사용자명
   password: 'test1234', // MySQL 비밀번호
   database: 'webapp_db'   // 사용할 데이터베이스 이름
